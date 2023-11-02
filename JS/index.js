16749bc7f6dc2c3fa65bf6d53aa7337c3cec2ae3
anderson <andinlucas8@gmail.com>
2023-11-02 14:32:06 -0300

    FEAT(carrossel)/ mudando novamente lógica do botão

--- JS/index.js
+++ JS/index.js
@@ -1,22 +1,51 @@
-const btnAnterior = document.getElementById("prev");
-const btnProximo = document.getElementById("next");
-const conteudoCarrossel = document.querySelector(".container-main-pagina1");
-const totalPaginas = 2;
+// const btnAnterior = document.getElementById("anterior");
+// const btnProximo = document.getElementById("proximo");
+// const conteudoCarrossel = document.querySelector(".container-main-pagina1");
+// const totalPaginas = 2;
 
-let paginaAtual = 1;
+// let paginaAtual = 1;
 
-const mostrarPagina = (pagina) => {
-    const deslocamento = (pagina - 1) * 50;
-    conteudoCarrossel.style.transform = `translateX(-${deslocamento}%)`;
+// const mostrarPagina = (pagina) => {
+//     const deslocamento = (pagina - 1) * 50;
+//     conteudoCarrossel.style.transform = `translateX(-${deslocamento}%)`;
+// }
+
+// btnAnterior.addEventListener("click", () => {
+//     paginaAtual = (paginaAtual - 1 + totalPaginas) % totalPaginas || totalPaginas;
+//     mostrarPagina(paginaAtual);
+// });
+
+// btnProximo.addEventListener("click", () => {
+//     paginaAtual = (paginaAtual % totalPaginas) + 1;
+//     mostrarPagina(paginaAtual);
+// });
+
+// mostrarPagina(paginaAtual);
+
+
+const btnAnterior = document.getElementById("anterior");
+const btnProximo = document.getElementById("proximo");
+const pages = document.querySelectorAll(".page");
+
+let paginaAtual = 0;
+
+function mostrarPagina(pagina) {
+    pages.forEach((page, index) => {
+        if (index === pagina) {
+            page.style.display = "block";
+        } else {
+            page.style.display = "none";
+        }
+    });
 }
 
 btnAnterior.addEventListener("click", () => {
-    paginaAtual = (paginaAtual - 1 + totalPaginas) % totalPaginas || totalPaginas;
+    paginaAtual = (paginaAtual - 1 + pages.length) % pages.length;
     mostrarPagina(paginaAtual);
 });
 
 btnProximo.addEventListener("click", () => {
-    paginaAtual = (paginaAtual % totalPaginas) + 1;
+    paginaAtual = (paginaAtual + 1) % pages.length;
     mostrarPagina(paginaAtual);
 });
 
