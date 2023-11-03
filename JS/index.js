6443f7c76e4b714daafdf28c163e6363f7b8951c
anderson <andinlucas8@gmail.com>
2023-11-03 09:29:59 -0300

    FEAT/ estilizando o botão

--- JS/index.js
+++ JS/index.js
@@ -1,35 +1,10 @@
-// const btnAnterior = document.getElementById("anterior");
-// const btnProximo = document.getElementById("proximo");
-// const conteudoCarrossel = document.querySelector(".container-main-pagina1");
-// const totalPaginas = 2;
-
-// let paginaAtual = 1;
-
-// const mostrarPagina = (pagina) => {
-//     const deslocamento = (pagina - 1) * 50;
-//     conteudoCarrossel.style.transform = `translateX(-${deslocamento}%)`;
-// }
-
-// btnAnterior.addEventListener("click", () => {
-//     paginaAtual = (paginaAtual - 1 + totalPaginas) % totalPaginas || totalPaginas;
-//     mostrarPagina(paginaAtual);
-// });
-
-// btnProximo.addEventListener("click", () => {
-//     paginaAtual = (paginaAtual % totalPaginas) + 1;
-//     mostrarPagina(paginaAtual);
-// });
-
-// mostrarPagina(paginaAtual);
-
-
 const btnAnterior = document.getElementById("anterior");
 const btnProximo = document.getElementById("proximo");
 const pages = document.querySelectorAll(".page");
 
 let paginaAtual = 0;
 
-function mostrarPagina(pagina) {
+const mostrarPagina = (pagina) => {
     pages.forEach((page, index) => {
         if (index === pagina) {
             page.style.display = "block";
