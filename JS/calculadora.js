56ac941342c3069b33b3abb64b0b6b5056149623
anderson <andinlucas8@gmail.com>
2023-11-08 19:39:06 -0300

    FEAT(calculadora)/ retomando o script

--- JS/calculadora.js
+++ JS/calculadora.js
@@ -1,3 +1,5 @@
-const CalcularImc = () {
-    
+const CalcularImc = () => {
+    const peso = parseFloat(document.getElementById('peso').value);
+    const altura = parseFloat(document.getElementById('altura').value);
+    const imc = peso / (altura * altura);
 }
