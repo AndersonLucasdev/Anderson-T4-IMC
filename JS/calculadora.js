4bc6b398256c6365bdca3fc626da0d3880561c52
anderson <andinlucas8@gmail.com>
2023-11-09 19:56:44 -0300

    FEAT(calculadora)/ mudando estrutura do script

--- JS/calculadora.js
+++ JS/calculadora.js
@@ -1,5 +1,7 @@
 const CalcularImc = () => {
-    const peso = parseFloat(document.getElementById('peso').value);
-    const altura = parseFloat(document.getElementById('altura').value);
-    const imc = peso / (altura * altura);
-}
+  const peso = parseFloat(document.getElementById("peso").value);
+  const altura = parseFloat(document.getElementById("altura").value);
+  const idade = parseInt(document.getElementById("idade").value);
+  const genero = document.getElementById("genero").value;
+  const imc = peso / (altura * altura);
+};
